5f665af6c6fe1905a765c587bc4c9b9554797ffc
caveradam <caveradam@users.noreply.github.com>
2016-06-27 15:39:21 -0400

    Add files via upload

---
+++ StepperSketch.ino
@@ -0,0 +1,14 @@
+#include <Stepper.h>
+
+const int stepsPerRevolution = 400;  // change this to fit the number of steps per revolution
+// for your motor
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
