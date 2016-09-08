e762fa3144ac38661d20aacc5bca36d65b0bbf52
caveradam <caveradam@users.noreply.github.com>
2016-09-07 20:42:22 -0400

    Add files via upload

--- OpenLidarMain.ino
+++ OpenLidarMain.ino
@@ -1,22 +1,54 @@
 /*     Open Lidar Project Code
  *     Author: Adam Sampson
  *     
+ *     SD card attached to the SPI BUss
+ *       MOSI - Pin 11
+ *       MISO - Pin 12
+ *       CLK - Pin 13
+ *       CS - Pin 10
+ *     
+ *     Bluetooth Connected to the TX/RX pins
+ *       TX - Pin 1 on Arduino to RX on Bluetooth Module
+ *       RX - Pin 0 on Artuino to TX on Bluetooth Module
+ *     
+ *     Hall Effect digital out connected to the A0 pin
+ *       D0 - Pin A3
+ *     
+ *     Motor 1 connected to digital output pins
+ *       Step - Pin 3
+ *       Direction - Pin 4
+ *     
+ *     Motor 2 connected to digital output pins
+ *       Step - Pin 5
+ *       Direction - Pin 6
+ *     
+ *     SF30-B connected to digital input/output pins through SoftwareSerial
+ *       ? - Pin A0 as Input
+ *       ? - Pin A1 as Ouptput
  */
 
-// #define DEBUG
+#include <SPI.h>
+#include <SD.h>
+
+#define DEBUG
 
 #ifdef DEBUG
- #define DEBUG_PRINT(x)  Serial.println (x)
- #define BAUD (115200)
+ #define DEBUG_PRINTLN(x)  Serial.println (x)
+ #define DEBUG_PRINT(x) Serial.print (x)
 #else
  #define DEBUG_PRINT(x)
+ #define DEBUG_PRINTLN(x)
 #endif
 
+#define BAUD (9600)
+
 // define pins numbers
-const int stepPin1 = 3; 
-const int dirPin1 = 4; 
-const int stepPin2 = 5; 
-const int dirPin2 = 6; 
+const int stepPin1 = 3; // motor 1 step pin
+const int dirPin1 = 4;  // motor 1 direction pin
+const int stepPin2 = 5; // motor 2 step pin 
+const int dirPin2 = 6;  // motor 2 direction pin
+const int digHall = A3; // digital hall effect pin
+//const int anaHall = A2; // analog hall effect pin
 
 // define motor profile
 const int riseDelay = 250;
@@ -26,24 +58,35 @@ int myPitchSteps = 400 * 4;  // This is a 400 step motor on quarter step mode
 
 void setup() {
   // put your setup code here, to run once:
-  #ifdef DEBUG
-    Serial.begin(BAUD);
-  #endif
+  // Set serial for bluetooth
+  Serial.begin(BAUD);
 
-  // Sets the two pins as Outputs
+  // Sets the motor driver pins as Outputs
   pinMode(dirPin1,OUTPUT);
   pinMode(stepPin1,OUTPUT); 
   pinMode(dirPin2,OUTPUT);
   pinMode(stepPin2,OUTPUT); 
+
+  pinMode(digHall,INPUT);
+  // Sets the 
 }
 
 void loop() {
-    unsigned long microcounter;
-    microcounter = micros();
+  // Set variables
+  boolean digHallState = 0;
+  int anHallState = 0;
+  
+  unsigned long microcounter;
+  microcounter = micros();
 
-  delay(10000); // 10 second delay to get hands out of the way
+  //delay(10000); // 10 second delay to get hands out of the way
+
+  // Collect the hall effect state
+  digHallState = digitalRead(digHall);
+  anHallState = analogRead(anaHall);
 
 // For each full rotation of the pitch motor run the yaw motor one step
+/*
 for(int yy = 0; yy < myYawSteps; yy++) { 
     // Run the Pitch motor forward 1 partial rotation
     for(int x = 0; x < myPitchSteps; x++) {
@@ -56,6 +99,12 @@ for(int yy = 0; yy < myYawSteps; yy++) {
       StepMotorForward(stepPin2,dirPin2);
       delayMicroseconds(250);
     }
+*/
+
+  DEBUG_PRINT("Digital Hall State: ");
+  DEBUG_PRINTLN(digHallState);
+  //DEBUG_PRINT(" , ");
+  //DEBUG_PRINTLN(anHallState);
 }
 
 void StepMotorForward(int myStepPin, int myDirPin){
